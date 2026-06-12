541aea900f101562f4c538efba88adedbf334abd
Sai Kumar <kumarsaisurisetty333@gmail.com>
2026-06-12 23:29:59 +0530

    added the shared environment files

---
+++ terraform/aws/envs/shared/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+module "network" {
+  source               = "../../modules/network"
+  env                  = "shared"
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  azs                  = var.azs
+}
+
+module "ecr" {
+  source       = "../../modules/ecr"
+  env          = "shared"
+  repositories = var.ecr_repositories
+}
