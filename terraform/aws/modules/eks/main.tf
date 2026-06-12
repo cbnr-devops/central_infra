1484a6bb202dd1592638c001bb950f3e34bb366d
Sai Kumar <kumarsaisurisetty333@gmail.com>
2026-06-12 16:40:10 +0530

    updated eks module

--- terraform/aws/modules/eks/main.tf
+++ terraform/aws/modules/eks/main.tf
@@ -1,35 +1,88 @@
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "19.21.0"
-
-  cluster_name    = var.cluster_name
-  cluster_version = "1.29"
-
-  vpc_id     = var.vpc_id
-  subnet_ids = var.subnet_ids
-
-  enable_irsa                    = true
-  cluster_endpoint_public_access = true
-
-  manage_aws_auth_configmap = true
-
-  aws_auth_users = [
+locals {
+  common_tags = merge(
     {
-      userarn  = "arn:aws:iam::312018064574:user/ec2-cli-user"
-      username = "ec2-cli-user"
-      groups   = ["system:masters"]
-    }
-  ]
+      Environment = var.env
+      Project     = "central-infra"
+    },
+    var.tags
+  )
+}
 
-  create_kms_key            = false
-  cluster_encryption_config = []
+resource "aws_iam_role" "cluster" {
+  name = "central-${var.env}-${var.cluster_name}-cluster-role"
 
-  eks_managed_node_groups = {
-    default = {
-      instance_types = var.instance_types
-      min_size       = 2
-      max_size       = 3
-      desired_size   = 2
-    }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "eks.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+
+  tags = local.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
+  role       = aws_iam_role.cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role" "node" {
+  name = "central-${var.env}-${var.cluster_name}-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+
+  tags = local.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEKSWorkerNodePolicy" {
+  role       = aws_iam_role.node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryReadOnly" {
+  role       = aws_iam_role.node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEKS_CNI_Policy" {
+  role       = aws_iam_role.node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_eks_cluster" "this" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.cluster.arn
+  version  = var.cluster_version
+
+  vpc_config {
+    subnet_ids = var.private_subnet_ids
   }
+
+  tags = local.common_tags
+}
+
+resource "aws_eks_node_group" "this" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "${var.cluster_name}-ng"
+  node_role_arn   = aws_iam_role.node.arn
+  subnet_ids      = var.private_subnet_ids
+
+  scaling_config {
+    desired_size = var.desired_capacity
+    min_size     = var.min_size
+    max_size     = var.max_size
+  }
+
+  instance_types = [var.instance_type]
+
+  tags = local.common_tags
 }
--- terraform/aws/modules/eks/outputs.tf
+++ terraform/aws/modules/eks/outputs.tf
@@ -1,7 +1,19 @@
 output "cluster_name" {
-  value = module.eks.cluster_name
+  description = "EKS cluster name"
+  value       = aws_eks_cluster.this.name
 }
 
 output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
+  description = "EKS cluster API server endpoint"
+  value       = aws_eks_cluster.this.endpoint
+}
+
+output "cluster_certificate_authority" {
+  description = "Base64 encoded certificate data required to communicate with cluster"
+  value       = aws_eks_cluster.this.certificate_authority[0].data
+}
+
+output "cluster_oidc_issuer" {
+  description = "OIDC issuer URL for IRSA"
+  value       = aws_eks_cluster.this.identity[0].oidc[0].issuer
 }
