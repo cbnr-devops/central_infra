122646993df59adcaf4036cd35496725ba756f9a
Sai Kumar <kumarsaisurisetty333@gmail.com>
2026-06-13 14:56:28 +0530

    Updated the dev and staging environment files

--- terraform/aws/envs/dev/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-aws_region     = "ap-southeast-2"
-vpc_cidr   = "10.0.0.0/16"
-instance_types = ["t3.small"]
--- terraform/aws/envs/staging/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "central-infra-tf-state"
-    key            = "staging/terraform.tfstate"
-    region         = "ap-southeast-2"
-    encrypt        = true
-  }
-}
